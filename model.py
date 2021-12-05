40b0c8591e696de67e88938a42882a74eab8b516
RomanovIgnat <romanov.ignat.p@gmail.com>
2021-12-05 08:04:59 +0300

    resnext

--- model.py
+++ model.py
@@ -5,8 +5,7 @@ import torch.nn as nn
 class ClassifierModel(nn.Module):
     def __init__(self):
         super().__init__()
-        self.model = resnext50_32x4d(pretrained=False)
-        self.model.fc = nn.Linear(512, 200)
+        self.model = resnext50_32x4d(pretrained=False, num_classes=200)
 
     def forward(self, x):
         return self.model(x)
